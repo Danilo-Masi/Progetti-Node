551055332a2bdbe5abede25cb50905f7524c15e3
dmasiii <danilomasi999@gmail.com>
2025-09-06 17:55:43 +0200

    Node (Apogeo)

---
+++ 01-Node (Apogeo)/06-www/media-types.mjs
@@ -0,0 +1,24 @@
+const types = {
+    "text/html": ["html", "htm"],
+    "text/plain": ["txt", "text"],
+    "text/css": ["css"],
+    "application/javascript": ["js", "mjs"],
+    "application/json": ["json"],
+    "image/jpeg": ["jpeg", "jpg"],
+    "image/x-icon": ["ico"],
+    "audio/mpeg": ["mpga", "mp2", "mp2a", "mp3", "m2a", "m3a"],
+    "video/mp4": ["mp4", "mp4v", "mpg4"],
+    "video/mpeg": ["mpeg", "mpg", "mpe", "m1v", "m2v"],
+    "video/x-matroska": ["mkv", "mk3d", "mks"],
+    "video/x-msvideo": ["avi"],
+}
+
+const extMap = new Map();
+
+for (let [type, extensions] of Object.entries(types)) {
+    for (let extension of extensions) {
+        extMap.set(`.${extension}`, type);
+    }
+}
+
+export default extMap;
