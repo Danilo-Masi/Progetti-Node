fd59a7043fef5ee86fbc694763f7c8f3e28c9648
dmasiii <danilomasi999@gmail.com>
2025-10-24 18:47:38 +0200

    Chat server

--- 02-Chat server/chat-utils.mjs
+++ 02-Chat server/chat-utils.mjs
@@ -1,5 +1,54 @@
+// Funzione per verificare se due socket sono uguali
 function isSameSocket(s1, s2) {
     return (s1.remoteAddress === s2.remoteAddress && s1.remotePort === s2.remotePort);
 }
 
-export { isSameSocket };
+// Funzione che invia il messaggio (message) a tutta la lista di utenti connessi (sockets)
+function broadcastMessage(sockets, message) {
+    sockets.forEach((sock) => sock.write(message));
+}
+
+// Funzione che compara le sock (utente che invia il messaggio) con gli utenti nella lista di utenti connessi (sockets)
+// e ritorna una mappa filtrata che esclude l'utente che invia il messaggio
+function getSocktesExcluding(sockets, sock) {
+    return sockets.filter((s) => !isSameSocket(s, sock));
+}
+
+// Funzione che attraverso i regex elimina gli eventuali caratteri \n e \r dalla stringa
+function removeCRLF(str) {
+    return str.replace(/[\r\n]+$/, "");
+}
+
+//
+function socketToId(sock) {
+    return `${sock.remoteAddress}:${sock.remotePort}`;
+}
+
+//
+function setName(namesMap, sock, name) {
+    namesMap[socketToId(sock)] = name;
+}
+
+//
+function getName(namesMap, sock) {
+    return namesMap[socketToId(sock)];
+}
+
+// Funzione che stampa il messaggio inviato dall'utente su tutti i terminali connessi (tranne che proprio a colui che ha scritto il messaggio)
+function processMessage(sockets, sock, message) {
+    const cleanMsg = removeCRLF(message);
+
+    if (cleanMsg.startsWith("/nick ")) {
+        const oldName = getName(sock);
+        const [_, name] = cleanMsg.split(" ");
+        setName(sock, name);
+        broadcastMessage(sockets, `${oldName} is now ${name}\n`);
+    } else {
+        broadcastMessage(
+            getSocktesExcluding(sockets, sock),
+            `<${getName(sock)}> ${cleanMsg}\n`
+        );
+    }
+}
+
+export { processMessage, setName, getName, socketToId };
--- 02-Chat server/server.mjs
+++ 02-Chat server/server.mjs
@@ -1,5 +1,5 @@
 import net from "net";
-import { isSameSocket } from "./chat-utils.mjs";
+import { processMessage, setName, socketToId } from "./chat-utils.mjs";
 
 const port = 5050;
 const host = "127.0.0.1";
@@ -7,17 +7,14 @@ const host = "127.0.0.1";
 const server = net.createServer();
 
 let sockets = [];
+let nameMap = {};
 
 server.on("connection", function (sock) {
-    console.log(`CONNECTED: ${sock.remoteAddress}:${sock.remotePort}`); // Da qui capiamo l'indiirizzo IP e la porta del nuovo utente connesso
+    console.log(`CONNECTED: ${socketToId(sock)}`); // Da qui capiamo l'indiirizzo IP e la porta del nuovo utente connesso
     sockets.push(sock); // Inserisce la nuova sock (nuovo utente connesso) nella lista delle sockets (lista degli utenti connessi)
+    setName(sockets, sock, socketToId(sock));
     sock.on("data", function (data) {
-        const sender = `${sock.remoteAddress}:${sock.remotePort}`;
-        sockets.forEach((s) => {
-            if (!isSameSocket(s, sock)) {
-                s.write(`<${sender}> ${data}`); // Stampa sul terminale di ogni utente connesso (tranne di chi manda il messaggio) il messaggio inviato da uno degli utenti
-            }
-        })
+        processMessage(sockets, sock, data.toString());
     });
 });
 
